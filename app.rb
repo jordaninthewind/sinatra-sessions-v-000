ff3abfe49eb54c40216d3e59d64570e980d32927
Jordan Kline <jordan.kline@gmail.com>
2018-02-08 19:57:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -53,7 +53,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    session[:id] = nil
+    session[:id] = {}
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
